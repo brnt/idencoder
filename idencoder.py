42a630b02a16696cd5f2a30cb4a71b7d7445ad29
Brent Thomson <brentthomson@gmail.com>
2020-04-23 10:11:02 -0600

    cleaned up some documentation

--- idencoder.py
+++ idencoder.py
@@ -8,24 +8,29 @@ Python implementation for encoding (usually sequential) integer IDs.
 A bit-shuffling approach is used to avoid generating consecutive, predictable
 values. However, the algorithm is deterministic and will guarantee that no
 collisions will occur.
+
 The encoding alphabet is fully customizable and may contain any number of
 characters. By default, digits and lower-case letters are used, with
 some removed to avoid confusion between characters like o, O and 0. The
 default alphabet is shuffled and has a prime number of characters to further
 improve the results of the algorithm.
+
 The block size specifies how many bits will be shuffled. The lower BLOCK_SIZE
 bits are reversed. Any bits higher than BLOCK_SIZE will remain as is.
 BLOCK_SIZE of 0 will leave all bits unaffected and the algorithm will simply
 be converting your integer to a different base.
+
 The intended use is that incrementing, consecutive integers will be used as
 keys to generate the encoded IDs. For example, to create a new short URL (à la
 bit.ly), the unique integer ID assigned by a database could be used to generate
 the last portion of the URL by using this module. Or a simple counter may be
 used. As long as the same integer is not used twice, the same encoded value
 will not be generated twice.
+
 The module supports both encoding and decoding of values. The min_length
 parameter allows you to pad the encoded value if you want it to be a specific
 length.
+
 Sample Usage:
 >>> import idencoder
 >>> x = idencoder.encode(12)
@@ -34,22 +39,28 @@ LhKA
 >>> key = idencoder.decode(x)
 >>> print(key)
 12
+
 Use the functions in the top-level of the module to use the default encoder.
 Otherwise, you may create your own IdEncoder object and use its encode()
 and decode() methods.
+
 Author: Michael Fogleman
 License: MIT
 Link: http://code.activestate.com/recipes/576918/
+
 Changelog:
+
 2014-05-09 Eric Wald
 - condensed duplicate bit-scrambling logic
 - switched to a native padding function
 - removed recursion and extra division
 - removed exponentiation and enumeration
 - removed excess convenience functions
+
 2014-01-13 Brent Thomson
 - added random_alphabet() function
 - replaced main method with a useful one
+
 2013-10-11 Brent Thomson
 - minor bug fixes
 - minor code cleanup
